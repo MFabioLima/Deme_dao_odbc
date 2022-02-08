ea7662e425c098e731ebaaf10bc539fcbf841fb1
MFabioLima <seculo737@hotmail.com>
2022-02-08 12:34:37 -0300

    Departamento e entidades classe

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,13 @@
+package aplicacao;
+
+import model.entidadades.Departamento;
+
+public class Programa {
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Departamento obj = new Departamento(1, "Books");
+        System.out.println(obj);
+	}
+
+}
